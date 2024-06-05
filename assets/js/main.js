aa5b3f31be45642adecaea20798e8e72077ddecd
Gayoso Facundo <facusbrr@gmail.com>
2024-06-05 14:30:41 -0300

    Se hicieron los estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ async function getDefinition() {
         const respuesta = await fetch(apiUrl);
         const info = await respuesta.json();
 
-        const definition = info[0]?.meanings[0]?.definitions[0]?.definition || 'No se encontró la definición.';
+        const definition = info[0]?.meanings[0]?.definitions[0]?.definition || 'No se encontró la definición';
         document.getElementById('definition').textContent = definition;
     } catch (error) {
         console.error('Error al obtener la definición:', error);
