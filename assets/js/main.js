280d04eb6df0ea96007cebb533f58a41e5338cd9
Gayoso Facundo <facusbrr@gmail.com>
2024-06-05 12:20:16 -0300

    Implementación de la API y funcionalidad terminada para que funcione

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+async function getDefinition() {
+    const word = document.getElementById('wordInput').value;
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    try {
+        const respuesta = await fetch(apiUrl);
+        const info = await respuesta.json();
+
+        const definition = info[0]?.meanings[0]?.definitions[0]?.definition || 'No se encontró la definición.';
+        document.getElementById('definition').textContent = definition;
+    } catch (error) {
+        console.error('Error al obtener la definición:', error);
+    }
+}
